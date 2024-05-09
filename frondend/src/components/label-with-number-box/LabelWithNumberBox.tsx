445e3061524e8e6a3efa0d502e81243e06c59ff9
GuyGPL <poramutinb@hotmail.com>
2024-05-09 22:41:40 +0700

    feat(frontend): create LabelWithNumberBox component

---
+++ frondend/src/components/label-with-number-box/LabelWithNumberBox.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from "react";
+import "./LabelWithNumberBox.scss";
+import { IconType } from "react-icons";
+
+type LabelWithNumberBoxProps = {
+    icon: IconType;
+    label: string;
+    number: number;
+    bgColor: string;
+};
+
+export default function LabelWithNumberBox({
+    icon,
+    label,
+    number,
+    bgColor,
+}: LabelWithNumberBoxProps): ReactElement {
+    const Icon = icon;
+
+    return (
+        <div className={`container color-${bgColor}`}>
+            <Icon size={40} />
+            <div className="text">{label}</div>
+            <div className="number">{number}</div>
+        </div>
+    );
+}
