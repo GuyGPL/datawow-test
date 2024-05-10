4004b1818472067e90df4716e20a64ae4cabfdc3
GuyGPL <poramutinb@hotmail.com>
2024-05-10 22:08:47 +0700

    feat(frontend): :sparkles: add CustomModal component

---
+++ frondend/src/components/custom-modal/CustomModal.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import "./CustomModal.scss";
+import { MdCancel } from "react-icons/md";
+
+type CustomModalProps = {
+    visible: boolean;
+    description: string;
+    leftButtonLabel: string;
+    rightButtonLabel: string;
+    onLeftButtonClick: () => void;
+    onRightButtonClick: () => void;
+};
+
+export default function CustomModal({
+    visible,
+    description,
+    leftButtonLabel,
+    rightButtonLabel,
+    onLeftButtonClick,
+    onRightButtonClick,
+}: CustomModalProps) {
+    if (!visible) return null;
+    return (
+        <div className="modal-container">
+            <div className="modal">
+                <div className="modal-header">
+                    <MdCancel size={40} color="red" />
+                </div>
+                <div className="modal-content">
+                    <p>Are you sure to delete?</p>
+                    <p>{description}</p>
+                </div>
+                <div className="modal-footer">
+                    <button
+                        className="button button-left"
+                        onClick={onLeftButtonClick}
+                    >
+                        {leftButtonLabel}
+                    </button>
+                    <button
+                        className="button button-right"
+                        onClick={onRightButtonClick}
+                    >
+                        {rightButtonLabel}
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
