406dc9d4fe707746c2638787a2361dc5e2f14ef3
GuyGPL <poramutinb@hotmail.com>
2024-05-11 22:45:59 +0700

    feat(frontend): :lipstick: update notification component styles

--- frondend/src/components/notification/Notification.tsx
+++ frondend/src/components/notification/Notification.tsx
@@ -1,12 +1,23 @@
 import { useNotification } from "@/contexts/NotificationContext";
 import React from "react";
+import { STATES } from "@/constants/notification-type";
+import { MdCancel } from "react-icons/md";
+import "./Notification.scss";
+import { FaCheckCircle } from "react-icons/fa";
 
 export default function Notification() {
-    const { notification, message } = useNotification();
+    const { notificationType, message } = useNotification();
     return (
-        notification !== null && (
-            <div className={notification}>
-                <p>{message}</p>
+        notificationType && (
+            <div className={`notification ${notificationType}`}>
+                <div className="notification-context">
+                    {notificationType === STATES.SUCCESS ? (
+                        <FaCheckCircle />
+                    ) : (
+                        <MdCancel />
+                    )}
+                    <span className="message">{message}</span>
+                </div>
             </div>
         )
     );
