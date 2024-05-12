dd2e4f6fba96a12f19eab56b1904b7e8443f86ad
GuyGPL <poramutinb@hotmail.com>
2024-05-12 13:37:59 +0700

    feat(frontend): :sparkles: create concert api call function

--- frondend/src/services/concerts.service.ts
+++ frondend/src/services/concerts.service.ts
@@ -1,17 +1,29 @@
+import {
+    ConcertListResponse,
+    ConcertSummaryResponse,
+    CreateConcertBodyRequest,
+} from "@/types/concerts.dto";
 import axios from "axios";
 
-export const fetchConcertList = async (): Promise<unknown> => {
+export const fetchConcertList = async (): Promise<ConcertListResponse> => {
     return await axios.get("/concerts").then(({ data }) => data);
 };
 
-export const fetchConcertSummary = async (): Promise<unknown> => {
-    return await axios.get("/concerts/summary").then(({ data }) => data);
-};
+export const fetchConcertSummary =
+    async (): Promise<ConcertSummaryResponse> => {
+        return await axios.get("/concerts/summary").then(({ data }) => data);
+    };
 
 export const removeConcert = async (concertId: string): Promise<unknown> => {
     return await axios
-        .delete(`/api/concerts/${concertId}`, {
+        .delete(`/concerts/${concertId}`, {
             headers: { "Content-Type": "application/json" },
         })
         .then(({ data }) => data);
+};
+
+export const createConcert = async (
+    body: CreateConcertBodyRequest
+): Promise<unknown> => {
+    return await axios.post(`/concerts/create`, body).then(({ data }) => data);
 };
