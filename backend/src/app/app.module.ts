37e56a44fd848ca7704fc027a89d091b6af0825d
GuyGPL <poramutinb@hotmail.com>
2024-05-13 02:43:24 +0700

    feat(backend): :sparkles: import all resource into app module

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,8 +3,17 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "src/database/database.module";
+import { ConcertsModule } from "src/concerts/concerts.module";
+import { ReservationsModule } from "src/reservations/reservations.module";
+import { UsersModule } from "src/users/users.module";
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        DatabaseModule,
+        ConcertsModule,
+        ReservationsModule,
+        UsersModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
