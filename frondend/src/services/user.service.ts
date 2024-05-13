e0b721ab86c265c2a71897e88ad151d00b4bb8b5
GuyGPL <poramutinb@hotmail.com>
2024-05-13 23:00:32 +0700

    refactor(frontend): :fire: remove unused user service

--- frondend/src/services/user.service.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const fetchUsersHistory = async (): Promise<unknown> => {
-    return await axios.get("/users/history").then(({ data }) => data);
-};
-
-export const fetchUserHistory = async (user: string): Promise<unknown> => {
-    return await axios.get(`/users/${user}/history`).then(({ data }) => data);
-};
