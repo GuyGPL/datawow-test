29da2f2c7f62bdefbcaaa8f859005687e8337f6c
GuyGPL <poramutinb@hotmail.com>
2024-05-13 15:48:16 +0700

    refactor(backend): :fire: remove unsed user resources

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "src/app/database/database.module";
 import { ConcertsModule } from "src/app/concerts/concerts.module";
 import { ReservationsModule } from "src/app/reservations/reservations.module";
-import { UsersModule } from "src/app/users/users.module";
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         DatabaseModule,
         ConcertsModule,
         ReservationsModule,
-        UsersModule,
     ],
     controllers: [AppController],
     providers: [AppService],
