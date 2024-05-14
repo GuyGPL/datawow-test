48c950d8be85a2b3b177e1ca832a4746aada5157
GuyGPL <poramutinb@hotmail.com>
2024-05-14 22:34:39 +0700

    style(frontend): :lipstick: style button on form add placeholder

--- frondend/src/components/concert-form-box/ConcertFormBox.tsx
+++ frondend/src/components/concert-form-box/ConcertFormBox.tsx
@@ -1,51 +1,61 @@
-import { FormEvent, ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import "./ConcertFormBox.scss";
-import { IoPersonOutline } from "react-icons/io5";
 import CustomButton from "../custom-button/CustomButton";
-import { FaMusic } from "react-icons/fa";
 import { useConcertForm } from "@/hooks/useConcertForm";
+import { LiaSave } from "react-icons/lia";
+import { GoPerson } from "react-icons/go";
 
 export default function ConcertFormBox(): ReactElement {
     const { setConcertName, setSeats, setDescription } = useConcertForm();
 
     return (
-        <form className="concert-form-box-container" onSubmit={undefined}>
+        <form className="concert-form-box-container">
             <div className="title">Create</div>
-            <div className="line"></div>
+            <div className="line" />
             <div className="concert-form">
-                <div className="form-input">
+                <div className="input-container">
                     <div className="form-label">Concert Name</div>
                     <input
                         type="text"
                         name="concertName"
                         className="form-input"
+                        placeholder="Please input concert name"
                         onChange={(e) => setConcertName(e.target.value)}
                     />
                 </div>
-                <div className="form-input">
+                <div className="input-container">
                     <div className="form-label">Total of seat</div>
                     <div className="input-with-icon">
                         <input
                             type="number"
                             name="totalSeats"
                             className="form-input"
+                            placeholder="Please input total of seat"
                             onChange={(e) => setSeats(e.target.value)}
                         />
-                        <FaMusic className="input-icon" />
+                        <GoPerson size={24} className="input-icon" />
                     </div>
                 </div>
-                <div className="form-input">
+                <div className="input-container">
                     <div className="form-label">Description</div>
                     <textarea
                         className="form-textarea"
                         rows={5}
                         name="description"
+                        placeholder="Please input description"
                         onChange={(e) => setDescription(e.target.value)}
                     ></textarea>
                 </div>
             </div>
-            <div></div>
-            <button type="submit">Save</button>
+            <div className="form-button">
+                <CustomButton
+                    icon={LiaSave}
+                    label="Save"
+                    buttonColor="blue"
+                    disabled={false}
+                    onClick={() => undefined}
+                />
+            </div>
         </form>
     );
 }
